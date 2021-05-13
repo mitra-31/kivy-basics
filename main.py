126fbb17d95eb90e7d9cebd204fde49711fdcfb2
mitra-31 <pavan.pushyamitra99@gmail.com>
2021-05-13 09:29:16 +0530

    Init commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+#MAin interface
+class MainWidget(Widget):
+    pass
+
+
+# Init app
+class DemoApp(App):
+    pass
+
+
+
+
+
+DemoApp().run()
